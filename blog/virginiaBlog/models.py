fcaa44f5cf54d3230aa498b08633bab177394f80
Virginia Lopez <virlop.017@gmail.com>
2022-06-21 10:29:06 -0300

    Agrego app

---
+++ blog/virginiaBlog/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Avatar(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="avatars", null=True, blank=True)
